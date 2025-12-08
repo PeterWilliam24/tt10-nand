40bad7211a359230f127e966fad03fec46ac0125
Peter William <peterwilliam@wayne.edu>
2025-12-07 21:44:27 -0500

    Fix comments in test.py for better clarity
    Corrected comments for clarity and consistency.

--- test/test.py
+++ test/test.py
@@ -19,11 +19,9 @@ dut._log.info("Test project behavior")
 # Set the input values you want to test
 dut.ui_in[0].value = 0
 dut.ui_in[1].value = 0
-# Wait for a certain number of clock cycles as you choose to see
-the output values
+# Wait for a certain number of clock cycles as you choose to see the output values
 await ClockCycles(dut.clk, 25)
-# The following assertion is just an example of how to check the
-output values.
+# The following assertion is just an example of how to check the output values.
 # Change it to match the actual expected output of your module:
 assert dut.uo_out[0].value == 1
 dut.ui_in[0].value = 0
