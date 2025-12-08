e4b050f2f6d5de087416353ce84e78180a9934e4
Peter William <peterwilliam@wayne.edu>
2025-12-07 21:52:38 -0500

    Rename module instance from tt_um_example to tt_um_peterwilliam24_nand

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_peter_william_nand user_project (
+  tt_um_peterwilliam24_nand user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
