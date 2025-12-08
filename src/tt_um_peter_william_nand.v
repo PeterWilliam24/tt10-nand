df029d9beca3e255aebf9eefcbbe6a5146106ea4
Peter William <peterwilliam@wayne.edu>
2025-12-07 21:51:08 -0500

    Update tt_um_peter_william_nand.v

--- src/tt_um_peter_william_nand.v
+++ src/tt_um_peter_william_nand.v
@@ -1,5 +1,5 @@
 `default_nettype none
-module tt_um_peter_william_nand (
+module tt_um_peterwilliam24_nand (
 input wire [7:0] ui_in, // Dedicated inputs
 output wire [7:0] uo_out, // Dedicated outputs
 input wire [7:0] uio_in, // IOs: Input path
